4a62ccbd9243707b49a1171ac4629b46db88d3a5
Melissa Skevington <melissa@hipmunk.com>
2016-08-23 08:37:15 -0700

    wait for all threads to finish before printing

--- solution_threads.py
+++ solution_threads.py
@@ -4,7 +4,6 @@ import comm_block
 
 
 ROOT = ""
-URLS_FOUND = set()
 
 
 def get_pw(page_name):
@@ -18,36 +17,36 @@ def get_pw(page_name):
     return page_password
 
 
-def print_pages(page_name, page_password, alread_seen, start=False):
+def get_urls(page_name, urls_found, threads=[]):
+    password = get_pw(page_name)
     resp = comm_block.rpc_json(comm.LINK_SERVER,
-                               dict(page=page_name, password=page_password))
+                               dict(page=page_name, password=password))
 
     if 'ok' not in resp:
         raise Exception("link server said: {!r}".format(resp))
 
     links = resp['ok']
 
-    for i, linked_page_name in enumerate(links):
-        URLS_FOUND.add(linked_page_name)
-
-        if linked_page_name not in alread_seen:
+    for linked_page_name in links:
+        if linked_page_name not in urls_found:
             print linked_page_name
 
-            alread_seen.append(linked_page_name)
-            new_pw = get_pw(linked_page_name)
+            urls_found.add(linked_page_name)
 
             thread = threading.Thread(
-                target=print_pages,
-                args=(linked_page_name, new_pw, alread_seen)
+                target=get_urls,
+                args=(linked_page_name, urls_found, threads)
             )
             thread.start()
 
-            if start:
-                thread.join()
+            threads.append(thread)
 
-    if start:
-        print "Number of urls found: {}".format(len(URLS_FOUND))
+    return urls_found, threads
 
 if __name__ == "__main__":
-    root_pw = get_pw(ROOT)
-    print_pages(ROOT, root_pw, [ROOT], start=True)
+    urls_found, threads = get_urls(ROOT, set([ROOT]))
+
+    for thread in threads:
+        thread.join()
+
+    print "{} total urls found".format(len(urls_found))
