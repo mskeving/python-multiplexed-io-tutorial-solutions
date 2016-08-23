d34e94c1bc9fedd6831fd2761877b43406ddd5b1
Melissa Skevington <melissa@hipmunk.com>
2016-08-22 23:55:24 -0700

    clean up web crawler

--- solution_web_crawler.py
+++ solution_web_crawler.py
@@ -3,7 +3,6 @@ import comm_block
 
 
 ROOT = ""
-URLS_FOUND = set()
 
 
 def get_pw(page_name):
@@ -17,9 +16,10 @@ def get_pw(page_name):
     return page_password
 
 
-def print_pages(page_name, page_password, already_seen, start=False):
+def get_urls(page_name, urls_found):
+    password = get_pw(page_name)
     resp = comm_block.rpc_json(comm.LINK_SERVER,
-                               dict(page=page_name, password=page_password))
+                               dict(page=page_name, password=password))
 
     if 'ok' not in resp:
         raise Exception("link server said: {!r}".format(resp))
@@ -27,20 +27,18 @@ def print_pages(page_name, page_password, already_seen, start=False):
     links = resp['ok']
 
     for linked_page_name in links:
-        URLS_FOUND.add(linked_page_name)
+        if linked_page_name not in urls_found:
+            urls_found.add(linked_page_name)
+            new_urls = get_urls(linked_page_name, urls_found)
+            urls_found.update(new_urls)
 
-        if linked_page_name not in already_seen:
-            print linked_page_name
-
-            already_seen.append(linked_page_name)
-            new_pw = get_pw(linked_page_name)
-
-            print_pages(linked_page_name, new_pw, already_seen)
-
-    if start:
-        print len(URLS_FOUND)
+    return urls_found
 
 
 if __name__ == "__main__":
-    root_pw = get_pw(ROOT)
-    print_pages(ROOT, root_pw, [ROOT], start=True)
+    urls_found = get_urls(ROOT, set([ROOT]))
+
+    for url in urls_found:
+        print url
+
+    print "{} total urls found".format(len(urls_found))
