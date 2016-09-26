99f59477e849aa35bf4f26e32354966ae954caac
Melissa Skevington <melissa@hipmunk.com>
2016-09-25 20:54:56 -0700

    send helper fnc, list with callbacks

--- comm_ev.py
+++ comm_ev.py
@@ -3,14 +3,13 @@ import comm_nonblock
 
 def recv(event_loop, sock, f):
     """Read from 'sock' until there's no more data to read.  When finished
-           call f(data)."""
+       call f(data)."""
     all_data = ""
 
     def read(all_data):
         data = comm_nonblock.recv(sock)
 
         if data is not None:
-            print "Got data piece: {!r}".format(data)
             all_data += data
             event_loop.on_read_ready(sock, lambda: read(all_data))
 
@@ -18,3 +17,19 @@ def recv(event_loop, sock, f):
             f(all_data)
 
     event_loop.on_read_ready(sock, lambda: read(all_data))
+
+
+def send(event_loop, sock, data, f):
+    """Send data until there's no more data to send. When finished
+       call f(data)."""
+
+    def send_data(all_data):
+        unsent_data = comm_nonblock.send(sock, all_data)
+
+        if unsent_data is not None:
+            event_loop.on_write_ready(sock, lambda: send_data(unsent_data))
+
+        else:
+            f(sock)
+
+    event_loop.on_write_ready(sock, lambda: send_data(data))
