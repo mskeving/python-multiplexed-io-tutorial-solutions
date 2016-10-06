7cdeb0801e7d47ba84337a876881c1662063986b
Melissa Skevington <melissa@hipmunk.com>
2016-10-06 11:06:42 -0700

    kannans

--- gen.py
+++ gen.py
@@ -1,40 +1,47 @@
 import select
 
-
 class EventLoop(object):
 
     def __init__(self):
-        self.readables = {}
-        self.writeables = {}
+        self.reads = {}
+        self.writes = {}
 
-    def step(self, gen):
-        try:
-            sock, signal = gen.next()
-            if signal == "send":
-                assert sock not in self.writeables, "sock already used"
-                self.writeables[sock] = gen
-            elif signal == "receive":
-                assert sock not in self.readables, "sock already used"
-                self.readables[sock] = gen
-            else:
-                raise Exception("Unknown signal {}".format(signal))
-        except StopIteration:
-            return
-
-    def run(self, gen_list):
-        for gen in gen_list:
+    def run(self, *gens):
+        for gen in gens:
             self.step(gen)
 
-        while self.readables or self.writeables:
-            rlist, wlist, xlist = select.select(
-                self.readables.keys(), self.writeables.keys(), ()
-            )
+        while len(self.reads) > 0 or len(self.writes) > 0:
+            rlist = self.reads.keys()
+            wlist = self.writes.keys()
+            rlist, wlist, xlist = select.select(rlist, wlist, rlist+wlist)
             assert len(xlist) == 0, "Got exception on socket."
 
             for sock in rlist:
-                gen = self.readables.pop(sock)
+                gen = self.reads.pop(sock)
                 self.step(gen)
 
             for sock in wlist:
-                gen = self.writeables.pop(sock)
+                gen = self.writes.pop(sock)
                 self.step(gen)
+
+    def step(self, gen):
+        try:
+            command = gen.next()
+            if isinstance(command, Read):
+                assert command.sock not in self.reads
+                self.reads[command.sock] = gen
+            elif isinstance(command, Write):
+                assert command.sock not in self.writes
+                self.writes[command.sock] = gen
+            else:
+                raise AssertionError("generator yielded unexpected value: {!r}".format(command))
+        except StopIteration:
+            pass
+
+class Read(object):
+    def __init__(self, sock):
+        self.sock = sock
+
+class Write(object):
+    def __init__(self, sock):
+        self.sock = sock
--- list_gen.py
+++ list_gen.py
@@ -1,52 +1,43 @@
-import comm
-import comm_nonblock
-import gen
 import json
 import sys
 
-from list_gen_send_helper import send_data, receive_data
-
-
-def print_password(page_name, resp):
-    data = b''.join(resp)
-
-    resp = json.loads(data)
-    if 'ok' not in resp:
-        raise Exception("link server said: {!r}".format(resp))
-
-    print "password for {}: {}".format(page_name, resp['ok'])
-
-
-def dumper(page_name):
-    sock = comm_nonblock.connect(comm.PASSWORD_SERVER)
-    password_resp = []
-
-    data = json.dumps(dict(page=page_name))
-
-    for sock, signal in send_data(sock, data):
-        yield sock, signal
-
-    for sock, signal, resp in receive_data(sock, data):
-        yield sock, signal
-        password_resp = resp
-
-    print_password(page_name, password_resp)
-
+import comm
+import comm_nonblock
+import gen
 
 def main():
     args = sys.argv[1:]
 
-    if len(args) < 1:
-        sys.stderr.write("Expecting one or more arguments, got None.")
+    if len(args) == 0:
+        sys.stderr.write("You must specify a list of pages on the command line.\n")
         sys.exit(1)
 
     event_loop = gen.EventLoop()
+    event_loop.run(*[get_pw(page_name) for page_name in args])
 
-    page_names = [arg for arg in args]
+def get_pw(page_name):
+    sock = comm_nonblock.connect(comm.PASSWORD_SERVER)
+    req = json.dumps(dict(page=page_name))
+    while True:
+        yield gen.Write(sock)
+        req = comm_nonblock.send(sock, req)
+        if req is None:
+            break
 
-    gens = [dumper(page) for page in page_names]
+    data_list = []
+    while True:
+        yield gen.Read(sock)
+        data_part = comm_nonblock.recv(sock)
+        if data_part is None:
+            break
+        data_list.append(data_part)
+    data = b''.join(data_list)
+    resp = json.loads(data)
 
-    event_loop.run(gens)
+    if 'ok' in resp:
+        print "{}: {}".format(page_name, resp['ok'])
+    else:
+        print "ERROR: {!r}".format(resp['error'])
 
 
 if __name__ == '__main__':
